967cf9c3974d65bf4506d892b341c24824ceacf1
Captain-Wet-Beard <45611892+Captain-Wet-Beard@users.noreply.github.com>
2024-11-12 22:35:01 -0500

    Reverted Steam Deck specific changes for Main, kept for steam_deck branch

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,18 @@ app.commandLine.appendSwitch("enable-gpu-rasterization")
 
 
 app.whenReady().then(() => {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    const x = Math.round((width * 1860)/1920)
+    const y = Math.round((height * 23)/1080)
+    const widthOverlay = Math.round((width * 40)/1920)
+    const heightOverlay = Math.round((height * 40)/1080)
+
     const mainWindow = new BrowserWindow({
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
             contextIsolation: false,
         },
-        width: 1280,
-        height: 800,
+        fullscreen: true,
         title: "Xbox Cloud Gaming",
     })
 
@@ -41,9 +46,19 @@ app.whenReady().then(() => {
     } else {
         mainWindow.setBackgroundColor("#1A1D1F")
         mainWindow.loadURL("https://www.xbox.com/en-US/play/launch/fortnite/BT5P2X999VH2")
-        mainWindow.maximize()
     }
 
+    const overlayWindow = new BrowserWindow({
+        x: x ,
+        y: y ,
+        width: widthOverlay ,
+        height: heightOverlay ,
+        frame: false,
+        transparent: true,
+        resizable:false,
+        parent: mainWindow ,
+    })
+    overlayWindow.loadURL("data:text/html,<input type='image' width='30' src='https://www.freeiconspng.com/uploads/silver-close-button-png-15.png' onclick='window.close()'/>")
 
     mainWindow.focus()
     
@@ -58,9 +73,8 @@ app.whenReady().then(() => {
         })
     }
 
-    globalShortcut.register("F11", () => {
-        const win = BrowserWindow.getAllWindows()[0]
-        win.setFullScreen(!win.isFullScreen())
+    overlayWindow.on("closed", async () => {
+        app.quit()
     })
 })
 
